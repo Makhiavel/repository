261bda29369f1cae817f24c9b8573605b7569ff1
Federicocc <federicoccarvalho@gmail.com>
2018-10-08 15:36:45 -0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,14 +1,30 @@
-#libraries needed
 import requests
 from bs4 import BeautifulSoup
 
-#creates a variable for my desired html page
-url = (r'http://g1.globo.com/ultimas-noticias.url', 'r')
-#should open the html and change the visualization of it
-with open(url) as html_file:
-	soup = BeautifulSoup(r.html_file, 'lxml')
-#scope the data goals
-article = soup.find('div', class_='article')
-print(article)
-#still working on the next steps
-headline = article.h2.a.text
+url = 'http://g1.globo.com/ultimas-noticias.html'
+r = requests.get(url)
+soup = BeautifulSoup(r.text, 'lxml')
+
+lista = soup.find_all('div', class_='bastian-feed-item')
+lista2= soup.find_all('a', class_='feed-post-link gui-color-primary gui-color-hover')
+links = soup.find_all('a', class_='feed-post-link gui-color-primary gui-color-hover')
+
+def scraper():
+	for item in links:
+		print ('---Link---')
+		print(item["href"]);
+	for item in lista2:
+		print ("---Título---")
+		print(item.contents[0]);
+	for item in lista:	
+		print ("---Resumo---")
+		print(item.contents[0].text);
+	
+	
+print("\n********Bem vindo ao G1 Webcrawler********\n")
+print("10 links, 10 titulos e 10 resumos respectivamente")
+
+print("Deseja visualizar?")
+input("Aperte Enter")
+scraper()
+	

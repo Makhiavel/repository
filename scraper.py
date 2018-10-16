9d640c23361fb33b0fe162e944753c947a724394
Federicocc <federicoccarvalho@gmail.com>
2018-10-16 15:25:50 -0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,30 +1,40 @@
-import requests
+import requests 
 from bs4 import BeautifulSoup
 
-url = 'http://g1.globo.com/ultimas-noticias.html'
-r = requests.get(url)
-soup = BeautifulSoup(r.text, 'lxml')
-
-lista = soup.find_all('div', class_='bastian-feed-item')
-lista2= soup.find_all('a', class_='feed-post-link gui-color-primary gui-color-hover')
-links = soup.find_all('a', class_='feed-post-link gui-color-primary gui-color-hover')
 
 def scraper():
-	for item in links:
-		print ('---Link---')
-		print(item["href"]);
-	for item in lista2:
-		print ("---Título---")
-		print(item.contents[0]);
-	for item in lista:	
-		print ("---Resumo---")
-		print(item.contents[0].text);
+	r = requests.get("http://g1.globo.com/ultimas-noticias.html")
+	soup = BeautifulSoup(r.content, 'lxml')
+	for link in soup.find_all('a', class_='feed-post-link gui-color-primary gui-color-hover'):
+		link.get("href")
+		print("\nTitles and links\n")
+		print (link.text)
+		print (link.get("href"))
+
 	
-	
-print("\n********Bem vindo ao G1 Webcrawler********\n")
-print("10 links, 10 titulos e 10 resumos respectivamente")
+		r = requests.get(link.get("href"))
+		soup = BeautifulSoup(r.content, 'lxml')
+		
+		for txt in soup.find_all('article'):
+			txt.get('articleBody')
+			print("\nContent\n")
+			print(txt.text)
+			break
+			
+		for time in soup.find_all('time'):
+			time.get('datetime')
+			print("\nData e horário\n")
+			print (time.text)
+			print("------------------------------------------------------------------------------------------------------------")
+			break
+
+print("\n*****************************Bem vindo ao G1 Webcrawler*****************************\n")
+print("O programa está organizado para vizualisar as últimas 10 noticias do site G1 da Globo\n")
+print("A ordem da visualização está em: Título, link, conteúdo completo e data com horário da postagem\n")
 
 print("Deseja visualizar?")
 input("Aperte Enter")
 scraper()
+
+input("Aperte Enter para sair")
 	
